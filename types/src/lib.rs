dd5df6c09b44e67ceaeb1a9e4237c896ae3db8f3
Bocar <59258164+bocarw121@users.noreply.github.com>
2022-10-07 11:49:43 -0400

    Update types library

--- types/src/lib.rs
+++ types/src/lib.rs
@@ -1,9 +1,13 @@
 use serde::{Deserialize, Serialize};
 
+#[derive(Serialize, Deserialize, PartialEq, Debug, Clone)]
+pub struct Team {
+    pub id: u32,
+    pub team: String,
+    pub city: String,
+}
 
 #[derive(Serialize, Deserialize, PartialEq, Debug, Clone)]
 pub struct NBATeams {
-  pub id: u32,
-  pub team: String,
-  pub city: String,
+    pub teams: Vec<Team>,
 }
