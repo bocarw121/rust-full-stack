316ae8f5f90c2a5b7af09d7325cd123484a3a24a
Bocar <59258164+bocarw121@users.noreply.github.com>
2022-10-07 12:33:41 -0400

    Update types

--- types/src/lib.rs
+++ types/src/lib.rs
@@ -3,7 +3,7 @@ use serde::{Deserialize, Serialize};
 #[derive(Serialize, Deserialize, PartialEq, Debug, Clone)]
 pub struct Team {
     pub id: u32,
-    pub team: String,
+    pub name: String,
     pub city: String,
 }
 
