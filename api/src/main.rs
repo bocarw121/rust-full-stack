ca316ce0d49f7c98c67309446f66f69050f6859c
Bocar <59258164+bocarw121@users.noreply.github.com>
2022-10-07 11:50:03 -0400

    Add nba teams route

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,10 +1,13 @@
 #[macro_use] extern crate rocket;
 
 pub mod routes;
+pub mod nba;
 use routes::*;
+
+
 
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build().mount("/", routes![index, all_nba_teams])
 }
--- api/src/routes/mod.rs
+++ api/src/routes/mod.rs
@@ -1,9 +1,16 @@
-// Export all the handler out
-#[get("/")] 
+use rocket::serde::json::Json;
+use types::NBATeams;
+
+use crate::nba;
+
+
+
+#[get("/")]
 pub(crate) fn index() -> &'static str {
     "Welcome to the NBA teams api"
 }
 
-
-
-
+#[get("/nba/teams")]
+pub(crate) fn all_nba_teams() -> Json<NBATeams> {
+    Json(NBATeams { teams: nba::get_teams() })
+}
